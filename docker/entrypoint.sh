8534e57131a8010573316f91b52e596bad4c4194
Konrad Rokicki <rokickik@janelia.hhmi.org>
2024-03-21 13:36:23 -0400

    fixed entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -euo pipefail
 source /opt/conda/etc/profile.d/conda.sh
 conda activate ngffbrowse
 cd /app/ngffbrowse || exit
@@ -11,7 +11,8 @@ PORT="${PORT:=8000}"
 # Export variables for ngffbrowse
 export DATA_URL="${DATA_URL:=/data}"
 
+set -x
 uvicorn ngffbrowse.serve:app --access-log \
     --workers $WORKERS --host $HOST --port $PORT \
-    --forwarded-allow-ips='*' --proxy-headers \ 
-     --ssl-keyfile "$KEY_FILE" --ssl-certfile "$CERT_FILE" "$@"
+    --forwarded-allow-ips='*' --proxy-headers \
+    --ssl-keyfile "$KEY_FILE" --ssl-certfile "$CERT_FILE" "$@"
