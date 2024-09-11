228c2f65e79767627a8de744335948ba06476976
Konrad Rokicki <rokickik@janelia.hhmi.org>
2024-09-11 16:37:37 -0400

    clean up ome-zarr code

--- zarrcade/agents/omezarr.py
+++ zarrcade/agents/omezarr.py
@@ -15,19 +15,38 @@ def get(mydict: dict, key: str, default: Any=None):
     return mydict[key] if key in mydict else default
 
 
-def _encode_image(zarr_path: str, image_group: zarr.Group):
-    multiscales = image_group.attrs['multiscales']
-    # TODO: what to do if there are multiple multiscales?
-    multiscale = multiscales[0]
-    axes = multiscale['axes']
+def _yield_color() -> Iterator[str]:
+    """ Yield a sequence of colors.
+    """
+    for c in ['magenta','green','cyan','white','red','green','blue']:
+        yield c
 
+
+def _encode_image(path: str, image_group: zarr.Group) -> Image:
+    """ Encode an image from an OME-Zarr group.
+    """
+    if 'multiscales' not in image_group.attrs:
+        raise ValueError(f"No multiscales found in group {image_group}")
+
+    multiscales = image_group.attrs['multiscales']
+    if len(multiscales) == 0:
+        raise ValueError(f"Empty multiscales in group {image_group}")
+    if len(multiscales) > 1:
+        # Currently, we only support a single multiscale image per group. See here for more info:
+        # https://forum.image.sc/t/ome-zarr-storage-structure-for-multiple-multiscale-images/71330
+        logger.warning(f"Multiple multiscales found in group {image_group}, using first")
+
+    multiscale = multiscales[0]
+    version = get(multiscale, 'version')
+    if version != '0.4':
+        raise ValueError(f"Unsupported multiscales version {version}")
+    
     # Use highest resolution
-    fullres_dataset = multiscale['datasets'][0]
-    fullres_path = fullres_dataset['path']
+    fullres = multiscale['datasets'][0]
+    fullres_path = fullres['path']
 
     group_path = image_group.name
-    array_path = group_path+'/'+fullres_dataset['path']
-    array_path, _ = re.subn('/+', '/', array_path)
+    array_path = re.sub('/+', '/', group_path+'/'+fullres_path)
 
     if array_path not in image_group:
         paths = ', '.join(image_group.keys())
@@ -36,8 +55,11 @@ def _encode_image(zarr_path: str, image_group: zarr.Group):
 
     array = image_group[array_path]
 
-    # TODO: we shouldn't assume a single transform
-    scales = fullres_dataset['coordinateTransformations'][0]['scale']
+    scale_transform = next((t for t in fullres['coordinateTransformations'] 
+                                       if t['type'] == 'scale'), None)
+    if not scale_transform:
+        raise ValueError("No scale transformation found in the full scale dataset")
+    scales = scale_transform['scale']
 
     axes_map = {}
     axes_names = []
@@ -47,7 +69,8 @@ def _encode_image(zarr_path: str, image_group: zarr.Group):
     chunks = []
     num_channels = 1
     num_timepoints = 1
-    for i, axis in enumerate(axes):
+
+    for i, axis in enumerate(multiscale['axes']):
         name = axis['name']
         axes_names.append(name)
         extent = array.shape[i]
@@ -56,13 +79,11 @@ def _encode_image(zarr_path: str, image_group: zarr.Group):
         unit = ''
         if axis['type']=='space':
             unit = axis['unit']
-            # TODO: better unit translation support
+            # TODO: add better unit translation support
             if unit in ['micrometer','micron']: unit = 'um'
             if unit=='nanometer': unit = 'nm'
-
             print_unit = unit
             if unit == 'um': print_unit = "μm"
-
             voxel_sizes.append("%.2f %s" % (round(scale,2), print_unit))
             dimensions.append("%.2f %s" % (round(extent * scale,2), print_unit))
         elif axis['type']=='channel':
@@ -77,12 +98,7 @@ def _encode_image(zarr_path: str, image_group: zarr.Group):
         chunks.append("%i" % chunk)
         axes_map[name] = Axis(name, scale, unit, extent, chunk)
 
-    def yield_color():
-        for c in ['magenta','green','cyan','white','red','green','blue']:
-            yield c
-
-    color_generator = yield_color()
-
+    color_generator = _yield_color()
     channels = []
     if 'omero' in image_group.attrs and 'channels' in image_group.attrs['omero']:
         for i, channel_meta in enumerate(image_group.attrs['omero']['channels']):
@@ -96,14 +112,14 @@ def _encode_image(zarr_path: str, image_group: zarr.Group):
                 contrast_limit_end = get(window, 'end')
             ))
     else:
+        # If there is no omero metadata, we do the best we can
         for i in range(num_channels):
             name = f"Ch{i}"
             color = next(color_generator)
             channels.append(Channel(name, color))
 
     return Image(
-        relative_path = zarr_path + group_path,
-        zarr_path = zarr_path,
+        relative_path = path,
         group_path = group_path,
         num_channels = num_channels,
         num_timepoints = num_timepoints,
@@ -119,6 +135,8 @@ def _encode_image(zarr_path: str, image_group: zarr.Group):
 
 
 def _yield_nested_image_groups(z: zarr.Group):
+    """ Recursively yield all nested image groups.
+    """
     for _,group in z.groups():
         if 'multiscales' in group.attrs:
             yield group
@@ -127,22 +145,17 @@ def _yield_nested_image_groups(z: zarr.Group):
 
 
 def _yield_image_groups(fs: Filestore, relative_path: str):
-    ''' Interrogates the OME-Zarr at the given URL and yields all of the 2-5D images within.
-    '''
+    """ Interrogates the OME-Zarr at the given URL and yields all of the images within.
+    """
     store = fs.get_store(relative_path)
     z = zarr.open(store, mode='r')
     # Based on https://ngff.openmicroscopy.org/latest/#bf2raw
     if 'bioformats2raw.layout' in z.attrs and z.attrs['bioformats2raw.layout']==3:
         if 'OME' in z:
-            series = z['OME'].attrs['series']
-            if len(series) == 1:
-                # We treat this as a single image for easier consumption
-                yield z[series[0]]
-            else:
-                # Spec: "series" MUST be a list of string objects,
-                # each of which is a path to an image group.
-                for image_id in series:
-                    yield z[image_id]
+            # Spec: "series" MUST be a list of string objects,
+            # each of which is a path to an image group.
+            for s in z['OME'].attrs['series']:
+                yield z[s]
         else:
             # Spec: If the "series" attribute does not exist and no "plate" is present:
             # - separate "multiscales" images MUST be stored in consecutively numbered
@@ -153,11 +166,12 @@ def _yield_image_groups(fs: Filestore, relative_path: str):
                 except IndexError:
                     break
     elif 'multiscales' in z.attrs:
+        # Top level is a single multiscale image
         yield z
     else:
+        # Search for multiscales in nested groups
         for group in _yield_nested_image_groups(z):
             yield group
-
 
 
 class OmeZarrAgent():
@@ -182,11 +196,4 @@ class OmeZarrAgent():
 
         with logger.catch(message=f"Failed to process {absolute_path}"):
             for image_group in _yield_image_groups(fs, path):
-                group_relpath = path
-                image_id = path
-                if image_group.path:
-                    gp = '/'+image_group.path
-                    image_id += gp
-                    group_relpath += gp
                 yield _encode_image(path, image_group)
-
