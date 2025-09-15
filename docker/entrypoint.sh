ceea6d58b83de67f644a27e9e5319bb9e76b37fa
Konrad Rokicki <rokickik@janelia.hhmi.org>
2025-09-15 16:29:42 -0400

    update docker image to use pixi as well

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /opt/conda/bin/activate zarrcade
 cd /app/zarrcade || exit
 
 # Set variables for uvicorn
@@ -12,7 +11,7 @@ export ZARRCADE_DATABASE__URL=$DB_URL
 export ZARRCADE_BASE_URL=$BASE_URL
 
 set -x
-uvicorn zarrcade.serve:app --access-log \
+pixi run uvicorn zarrcade.serve:app --access-log \
     --workers $WORKERS --host $HOST --port $PORT \
     --forwarded-allow-ips='*' --proxy-headers \
     --ssl-keyfile "$KEY_FILE" --ssl-certfile "$CERT_FILE" "$@"
