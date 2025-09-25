ef2e55cbfd0fca730d3691bf17c1b02cbc32bce6
Konrad Rokicki <rokickik@janelia.hhmi.org>
2025-09-24 21:02:59 -0400

    removed old release script

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export VERSION=$1
-docker build . --build-arg GIT_TAG=$VERSION -t ghcr.io/janeliascicomp/zarrcade:$VERSION \
-    && docker push ghcr.io/janeliascicomp/zarrcade:$VERSION \
-    && docker tag ghcr.io/janeliascicomp/zarrcade:$VERSION ghcr.io/janeliascicomp/zarrcade:latest \
-    && docker push ghcr.io/janeliascicomp/zarrcade:latest
-
